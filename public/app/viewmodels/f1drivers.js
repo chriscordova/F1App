3c98036cb575219150f63ceae3737655142ce99d
Chris Cordova <cordovac89@gmail.com>
2016-10-09 13:06:04 +1100

    added driver bio

--- public/app/viewmodels/f1drivers.js
+++ public/app/viewmodels/f1drivers.js
@@ -2,6 +2,7 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
 
     return {
         drivers: ko.observableArray([]),
+        biography: ko.observableArray([]),
         activate: function() {
             var that = this;
             
@@ -15,9 +16,11 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
 
             
         },
-        select: function(item) {
+        select: function(item,v,b,f) {
             //the app model allows easy display of modal dialogs by passing a view model
             //views are usually located by convention, but you an specify it as well with viewUrl
+            //app.biography(item.Biography);
+
             item.viewUrl = 'views/driverimage';
             app.showDialog(item);
         }
