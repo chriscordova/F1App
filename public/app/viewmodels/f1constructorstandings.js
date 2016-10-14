96e1a1b8f6cf234906bfc5ba31041b9039a67fdf
Chris Cordova <cordovac89@gmail.com>
2016-10-14 16:31:07 +1100

    optimized data and speed for constructors standings

--- public/app/viewmodels/f1constructorstandings.js
+++ public/app/viewmodels/f1constructorstandings.js
@@ -2,26 +2,65 @@ define(['plugins/http', 'durandal/app', 'knockout', 'plugins/router', 'datatable
 
     return {
         router: router,
+        constructorStandings: ko.observableArray([]),
         years: ko.observableArray([]),
-        constructors: ko.observableArray([]),
+        constructorsForYear: ko.observableArray([]),
+        constructorObject: ko.observable({ 'data':[] }),
         activate: function(year) {
             var that = this;
+
+            //NEW
+            var sURL = 'http://www.c0rdii.com/f1/api/standings/constructor/' + year;
+            var allTeams = [];
+            var aPromises = [];
+            var allYearTeams = [];
             var aYears = [{
                 "years":  ['2015','2016']
             }];
-
-            that.years(aYears[0].years);
             
-            var sURL = 'http://www.c0rdii.com/f1/api/standings/constructor/'+year;
-            var allTeams = [];
-            $.getJSON(sURL, function(data){
-                allTeams = data[0].ConstructorStandings;
-            }).done(function(){
-                buildDataTable('#constructorsTable', that.constructors, allTeams);
-            });            
+            if (that.constructorObject().data.length > 0){
+                if (year == null) return;
+
+                allYearTeams = that.getYearStandingsFromObject(that.constructorObject(), year);
+                buildDataTable('#constructorsTable', that.constructorsForYear, allYearTeams.constructorstandings);
+            }
+            else {
+                
+                that.years(aYears[0].years);
+                $(that.years()).each(function(i,v){
+                    var sStandingURL = 'http://www.c0rdii.com/f1/api/standings/constructor/' + v;
+                    var o = $.getJSON(sStandingURL, function(data){
+                            that.constructorObject().data.push({"year": v, "constructorstandings":  data[0].ConstructorStandings});
+                        });
+                    
+                    aPromises.push(o);
+                });
+
+                $.when.apply($, aPromises).done(function(){
+                    $(that.constructorObject().data).each(function(i,v){
+                        allTeams.push(v.constructorstandings);
+                    });
+
+                    that.constructorStandings(allTeams);
+
+                    if (year != null){
+                        allYearTeams = that.getYearStandingsFromObject(that.constructorObject(), year);
+                        buildDataTable('#constructorsTable', that.constructorsForYear, allYearTeams.constructorstandings);
+                    } 
+                });       
+            }            
         },
-        param: function(){
+        yearParam: function(){
             return router.activeInstruction().params[0];
+        },
+        getYearStandingsFromObject: function(obj, y){
+            var that = this;
+
+            var returnObj = $.grep(obj.data, function(s){
+                return s.year == y.toString();
+            });
+
+            return returnObj.first();
         }
     }
 });
